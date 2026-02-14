2623758c4c2f3ca34238b140eb3f99c7b267c4f4
Monte Lai <monte.lai@consensys.net>
2026-02-14 14:29:42 +0800

    fix: fix e2e

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   server: {
     port: 3002,
     host: true,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3335',
